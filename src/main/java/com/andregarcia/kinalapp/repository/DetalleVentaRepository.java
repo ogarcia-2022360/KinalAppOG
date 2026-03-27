f22a003a811b76163f854578315efc38bfb029b9
ogarcia-2022360 <ogarcia-2022360@kinal.edu.gt>
2026-03-27 12:14:27 -0600

    refactor: update DetalleVentaRepository repository

--- src/main/java/com/andregarcia/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/andregarcia/kinalapp/repository/DetalleVentaRepository.java
@@ -1,4 +1,7 @@
 package com.andregarcia.kinalapp.repository;
 
-public interface DetalleVentaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.andregarcia.kinalapp.entity.DetalleVenta;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
 }
