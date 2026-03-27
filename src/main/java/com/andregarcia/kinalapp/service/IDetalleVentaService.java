447e0e6ad29b2178feea2bfcc802b5812fe9fbc0
ogarcia-2022360 <ogarcia-2022360@kinal.edu.gt>
2026-03-27 13:40:45 -0600

    refactor: update IDetalleVentaService service

--- src/main/java/com/andregarcia/kinalapp/service/IDetalleVentaService.java
+++ src/main/java/com/andregarcia/kinalapp/service/IDetalleVentaService.java
@@ -1,4 +1,26 @@
 package com.andregarcia.kinalapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+
+import com.andregarcia.kinalapp.entity.DetalleVenta;
+
 public interface IDetalleVentaService {
+
+    //Metodo para listar los detalles de ventas
+    List<DetalleVenta> listarDetalleVenta();
+
+    //Metodo que guarda el detalle de una venta en la base de datos
+    DetalleVenta guardarDetalleVenta(DetalleVenta detalleVenta);
+
+    //Contenedor que puede o no tener valor
+    Optional<DetalleVenta> buscarDetalleVenta(Integer codigoDetalleVenta);
+
+    //Metodo para actualizar el detalle de una venta
+    DetalleVenta actualizarDetalleVenta(Integer codigoDetalleVenta, DetalleVenta detalleVenta);
+
+    //Metodo para eliminar el datalle de una venta
+    void eliminarDetalleVenta(Integer codigoDetalleVenta);
 }
