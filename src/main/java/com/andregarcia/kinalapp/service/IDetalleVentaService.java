f7e704b7f1d7b87dfc90491e208e30f4c1ecd44e
ogarcia-2022360 <ogarcia-2022360@kinal.edu.gt>
2026-03-27 13:42:10 -0600

    refactor: update IDetalleVentaService service

--- src/main/java/com/andregarcia/kinalapp/service/IDetalleVentaService.java
+++ src/main/java/com/andregarcia/kinalapp/service/IDetalleVentaService.java
@@ -3,8 +3,6 @@ package com.andregarcia.kinalapp.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
-
 import com.andregarcia.kinalapp.entity.DetalleVenta;
 
 public interface IDetalleVentaService {
