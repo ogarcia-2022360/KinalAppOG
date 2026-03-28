7edf57f095fafa51cbe5b4967fc6918735d665bb
ogarcia-2022360 <ogarcia-2022360@kinal.edu.gt>
2026-03-27 22:08:44 -0600

    refactor: update VentaController controller

--- src/main/java/com/andregarcia/kinalapp/controller/VentaController.java
+++ src/main/java/com/andregarcia/kinalapp/controller/VentaController.java
@@ -1,6 +1,5 @@
 package com.andregarcia.kinalapp.controller;
 
-
 import com.andregarcia.kinalapp.entity.Venta;
 import com.andregarcia.kinalapp.service.IVentaService;
 import org.springframework.http.HttpStatus;
@@ -10,23 +9,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/ventas")
- // las rutas de este controlador empizan por /ventas
+@RequestMapping("/ventas")
+// las rutas de este controlador empizan por /ventas
 public class VentaController {
+
     private final IVentaService ventaService;
-    public VentaController (IVentaService ventaService) {
+
+    public VentaController(IVentaService ventaService) {
         this.ventaService = ventaService;
     }
 
     // Para peticiones GET
     @GetMapping
-    public ResponseEntity<List<Venta>> listarVenta(){
+    public ResponseEntity<List<Venta>> listarVenta() {
         List<Venta> ventas = ventaService.listarVentas();
         return ResponseEntity.ok(ventas);
     }
 
     // {codigoVenta} es el valor a buscar
-    @GetMapping ("/{codigoVenta}")
+    @GetMapping("/{codigoVenta}")
     public ResponseEntity<Venta> buscarPorCodigoVenta(@PathVariable int codigoVenta) {
         return ventaService.buscarPorCodigoVenta(codigoVenta)
                 .map(ResponseEntity::ok)
@@ -34,11 +35,11 @@ public class VentaController {
     }
 
     @PostMapping
-    public ResponseEntity<?> guardarVenta(@RequestBody Venta venta){
-        try{
+    public ResponseEntity<?> guardarVenta(@RequestBody Venta venta) {
+        try {
             Venta nuevaVenta = ventaService.guardarVenta(venta);
             return new ResponseEntity<>(nuevaVenta, HttpStatus.CREATED);
-        }catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -55,23 +56,21 @@ public class VentaController {
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-
     }
 
-        //Actualizar la venta a traves del Codigo
-        @PutMapping("/{codigoVenta}")
-        public ResponseEntity<?> actualizarVenta(@PathVariable int codigoVenta, @RequestBody Venta venta){
-            try {
-                if (!ventaService.existePorCodigoVenta(codigoVenta)) {
-                    return ResponseEntity.notFound().build();
-                }
-                Venta ventaActualizada = ventaService.actualizarVenta(codigoVenta, venta);
-                return ResponseEntity.ok(ventaActualizada);
-            }catch (IllegalArgumentException e){
-                return ResponseEntity.badRequest().body(e.getMessage());
-            }catch (RuntimeException e){
+    //Actualizar la venta a traves del Codigo
+    @PutMapping("/{codigoVenta}")
+    public ResponseEntity<?> actualizarVenta(@PathVariable int codigoVenta, @RequestBody Venta venta) {
+        try {
+            if (!ventaService.existePorCodigoVenta(codigoVenta)) {
                 return ResponseEntity.notFound().build();
-
             }
+            Venta ventaActualizada = ventaService.actualizarVenta(codigoVenta, venta);
+            return ResponseEntity.ok(ventaActualizada);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
         }
+    }
 }
